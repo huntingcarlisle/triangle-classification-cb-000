b3444e3157ea1c6449e82cd04ba3da4ca2227afb
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-07 02:00:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,10 +9,16 @@ class Triangle
     @test_one = (@side_one == @side_two)
     @test_two = (@side_two == @side_three)
     @test_three = (@side_one == @side_three)
+    @test_four = ((@side_one + @side_two) < @side_three) ||
+                 ((@side_two + @side_three) < @side_one) ||
+                 ((@side_one + @side_three) < @side_two)
+    @test_five = @side_one <= 0 || @side_two <= 0 || @side_three <= 0
   end
   
   def kind
-    if @test_one && @test_two
+    if @test_four || @test_five
+      raise TriangleError
+    elsif @test_one && @test_two
       return :equilateral
     elsif @test_one || @test_two || @test_three
       return :isosceles
