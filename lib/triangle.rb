ec0a33d9cbfdb70e58dbd5a5f01352e219bc361d
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-07 01:53:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,19 @@ class Triangle
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
+    @test_one = (@side_one == @side_two)
+    @test_two = (@side_two == @side_three)
+    @test_three = (@side_one == @side_three)
   end
   
   def kind
-    if 
+    if @test_one && @test_two
+      return :equilateral
+    elsif @test_one || @test_two || @test_three
+      return :isosceles
+    else
+      return :scalene
+    end
   end
   
   class TriangleError < StandardError
